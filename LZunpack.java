d72c3c663999464701ff25338e179f990e9b9d99
ld137 <83894641+ld137@users.noreply.github.com>
2023-04-28 21:56:02 +1200

    First Implementation of the unpack
    Couldn't test as Pack isn't done yet... Will test and adjust as needed tomorrow (29th)

--- LZunpack.java
+++ LZunpack.java
@@ -1,1 +1,46 @@
-//Impliment
+import java.io.*;
+
+public class LZunpack {
+    public static void main(String[] args) throws IOException {
+
+        InputStream in = System.in;
+        int b;
+        int maxLen = 0;
+        String hexStream = "";
+        boolean loop = true;
+
+        while(loop){
+            b = in.read();
+
+            // get high bits
+            int highBits = (b >> 4) & 0xf;
+            // get low bits
+            int lowBits = b & 0xf;
+
+            if((lowBits & 0) != 0){
+                maxLen += 8;
+            }
+            else{
+                loop = false;
+            }
+
+            if((highBits & 0) == 0){
+                loop = false;
+            }
+        }
+
+        // Read input to end
+        while ((b = in.read()) != -1) {
+            for(int i = 0; i < maxLen / 8; i++){
+                // get high bits
+            int highBits = (b >> 4) & 0xf;
+            // get low bits
+            int lowBits = b & 0xf;
+            // convert them to a char and concat them
+            String hexDigit = Integer.toHexString(highBits) + Integer.toHexString(lowBits);
+            hexStream += hexDigit;
+            }
+
+        }
+    }
+}
