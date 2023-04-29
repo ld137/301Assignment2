982668be0926e2e422179cdee409ffb193703c8d
Maverick <jake.postlewaight@gmail.com>
2023-04-29 16:40:02 +1200

    Packer done (using cheat method)

--- LZpack.java
+++ LZpack.java
@@ -1,1 +1,72 @@
 //Impliment
+
+//Impliment
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.Scanner;
+
+public class LZpack {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<byte[]> outputBytes = new ArrayList<byte[]>();
+        ArrayList<String> lines = new ArrayList<>();
+        char stopChar = '$';
+        // Read input until the stop character is encountered
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            System.err.println(line);
+            if (line.indexOf(stopChar) != -1) {
+                // Stop character found, append input up to stop character and stop
+                lines.add(line.substring(0, line.indexOf(stopChar)) + stopChar);
+                break;
+            } else {
+                // Stop character not found, add line to the array
+                lines.add(line);
+            }
+        }
+        scanner.close();
+        String outputBinaryString = "";
+        int currentMaxDict = 0;
+        for (String line : lines) {
+            String[] parts = line.split(" ");
+            int phrase = Integer.parseInt(parts[0]);
+            int hexCode = Character.digit(parts[1].charAt(0), 16);
+            int log2x = (int) Math.ceil(Math.log(currentMaxDict == 0 ? 1 : currentMaxDict) / Math.log(2));
+            // int phraseMask = (1 << log2x) - 1;
+            int phraseMask = (1 << log2x) - (phrase == 0 ? 0 : 1);
+            int phraseResult = phrase & phraseMask;
+
+            int hexMask = 0b1111;
+            int hexResult = (hexCode == -1 ? 0 : hexCode) & hexMask;
+
+
+            //System.err.println("phrase:" + phrase);
+            //System.err.println("hexCode:" + hexCode);
+            //System.err.println("log2x:" + log2x);
+            String paddedHex = String.format("%1$" + 4 + "s", Integer.toBinaryString(hexResult)).replace(' ', '0');
+            //System.err.println("hexResult:" + paddedHex);
+            String paddedPhrase = String
+                    .format("%1$" + (log2x == 0 ? 1 : log2x) + "s", Integer.toBinaryString(phraseResult))
+                    .replace(' ', '0');
+            //System.err.println("phraseResult:" + paddedPhrase);
+            //System.err.println("");
+            outputBinaryString += paddedPhrase + paddedHex;
+            //System.err.println("");
+            currentMaxDict++;
+
+        }
+        System.err.println(outputBinaryString);
+
+        BitSet bitSet = new BitSet(outputBinaryString.length());
+
+        for (int i = 0; i < outputBinaryString.length(); i++) {
+            if (outputBinaryString.charAt(i) == '1') {
+                bitSet.set(i);
+            }
+        }
+
+        byte[] byteArray = bitSet.toByteArray();
+        System.out.print(byteArray);
+
+    }
+}
--- LZunpack.java
+++ LZunpack.java
@@ -8,32 +8,15 @@ public class LZunpack {
         int maxLen = 0;
         String hexStream = "";
         boolean loop = true;
-
-        //Loop through the input bits continuously until end of stream (in.read returns -1 when you get to the end)
-
-        //Inside the loop,
-        //We need to read in bytes until we get a byte that has any amount of '0s' in it
-        //IE b = in.read() < 255 
-        //counting each byte as we go over it.
-        //then we need to get the byte that has less than the maximum amount of 1's in it, and count the ON bits
-        //IE 11100000 we need to get the fact it has 3 on bits in it and add 3+ (8*the amount of bytes we have gone past that are all 1's)
-        //So our first byte stream might be like this 11111111 11111111 11100000
-        //We should get the number of ON bits from these 3 bytes, by having 2*8 from the first 2 and adding 3 from the third, meaning we have 19 bits
-        //that will be the phrase length for everything going forward, so P is the phrase and C is the hex going forward
-        // PPPPPPPP PPPPPPP PPPCCCCP PPPPPPP PPPPPPPP PPCCCCPP PPPPPPPP PPPPPPPP PCCCC etc etc
-        //With that value we have the phrase length going forward and from the next byte we can read x length as the phrase then the next 4 as the hex chars byte value
-        //continue until end of stream, 
-
-        //the output stream will be integers in the same format the encoder outputs in
-        //IE    MAXPHRASELENGTH a
-        //                    1 a
-        //                    2 a
-        // ...
-        //...
-        // $
+        
+        //While reading
+        //Take in the bytes, current dict = 1 until Ceil log P > 1
+        //bits 0-log p in the upper are the dict, and then next 4 are the hex
+        //rinse and repeat until end of stream, when you get to the end of stream, there might be leftover space, 
+        //so when reading in the 'final' phrase, if you get past the current log p value disregard progress and consider the read complete
 
 
-        while(loop){
+        while (loop) {
             b = in.read();
 
             // get high bits
@@ -41,14 +24,13 @@ public class LZunpack {
             // get low bits
             int lowBits = b & 0xf;
 
-            if((lowBits & 0) != 0){
+            if ((lowBits & 0) != 0) {
                 maxLen += 8;
-            }
-            else{
+            } else {
                 loop = false;
             }
-            
-            if((highBits & 0) == 0){
+
+            if ((highBits & 0) == 0) {
                 loop = false;
             }
         }
@@ -56,17 +38,16 @@ public class LZunpack {
         // Read input to end
         while ((b = in.read()) != -1) {
             hexStream = "";
-            for(int i = 0; i < maxLen / 8; i++){
+            for (int i = 0; i < maxLen / 8; i++) {
                 // get high bits
-            int highBits = (b >> 4) & 0xf;
-            // get low bits
-            int lowBits = b & 0xf;
-            // convert them to a char and concat them
-            String hexDigit = Integer.toHexString(highBits) + Integer.toHexString(lowBits);
-            hexStream += hexDigit;
+                int highBits = (b >> 4) & 0xf;
+                // get low bits
+                int lowBits = b & 0xf;
+                // convert them to a char and concat them
+                String hexDigit = Integer.toHexString(highBits) + Integer.toHexString(lowBits);
+                hexStream += hexDigit;
             }
             System.err.println(hexStream);
-        }
         }
     }
 }
