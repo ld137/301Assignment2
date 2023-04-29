7956e82ff564ab8202e2dbda0e1952635676beb9
Maverick <jake.postlewaight@gmail.com>
2023-04-29 12:35:23 +1200

    Notes to use to update

--- LZunpack.java
+++ LZunpack.java
@@ -9,6 +9,30 @@ public class LZunpack {
         String hexStream = "";
         boolean loop = true;
 
+        //Loop through the input bits continuously until end of stream (in.read returns -1 when you get to the end)
+
+        //Inside the loop,
+        //We need to read in bytes until we get a byte that has any amount of '0s' in it
+        //IE b = in.read() < 255 
+        //counting each byte as we go over it.
+        //then we need to get the byte that has less than the maximum amount of 1's in it, and count the ON bits
+        //IE 11100000 we need to get the fact it has 3 on bits in it and add 3+ (8*the amount of bytes we have gone past that are all 1's)
+        //So our first byte stream might be like this 11111111 11111111 11100000
+        //We should get the number of ON bits from these 3 bytes, by having 2*8 from the first 2 and adding 3 from the third, meaning we have 19 bits
+        //that will be the phrase length for everything going forward, so P is the phrase and C is the hex going forward
+        // PPPPPPPP PPPPPPP PPPCCCCP PPPPPPP PPPPPPPP PPCCCCPP PPPPPPPP PPPPPPPP PCCCC etc etc
+        //With that value we have the phrase length going forward and from the next byte we can read x length as the phrase then the next 4 as the hex chars byte value
+        //continue until end of stream, 
+
+        //the output stream will be integers in the same format the encoder outputs in
+        //IE    MAXPHRASELENGTH a
+        //                    1 a
+        //                    2 a
+        // ...
+        //...
+        // $
+
+
         while(loop){
             b = in.read();
 
@@ -23,7 +47,7 @@ public class LZunpack {
             else{
                 loop = false;
             }
-
+            
             if((highBits & 0) == 0){
                 loop = false;
             }
